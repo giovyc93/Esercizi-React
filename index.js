1277576060a14eb573aad5c5b899a35c6ad2a964
giovyc93 <giovyc93@gmail.com>
2023-03-08 00:38:29 +0100

    es

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { HelloWorld } from './helloWorld';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HelloWorld/>
+  </React.StrictMode>
+);
+
