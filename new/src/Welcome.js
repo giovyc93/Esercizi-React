3285c10559d439458b77acc917b55b16794dbe5f
giovyc93 <giovyc93@gmail.com>
2023-03-09 17:42:37 +0100

    es

--- new/src/Welcome.js
+++ new/src/Welcome.js
@@ -5,7 +5,7 @@ export class Welcome extends React.Component{
       }
         render(){
             return <div>
-            <p>Welcome, {this.props.name}</p>
+            <p>Welcome, <strong>{this.props.name}</strong></p>
             <p>Your age is {this.props.age}</p>
             </div>
     }
