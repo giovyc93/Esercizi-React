a954ddf2e07d8e00c26c211ad4dcd4042a4e297a
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-03-27 18:45:53 +0200

    nuovo

--- new/src/TodoList.js
+++ new/src/TodoList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 class TodoList extends React.Component{
-    state = {items :["HTML", "CSS", "JS", "REACT"],
+    state = {items :["HTML", "CSS", "JS"],
     newList : null,
     addBtn : true,
 
@@ -25,11 +25,18 @@ class TodoList extends React.Component{
 			items: []
 		});
     }
+    handleRemove = (event, remove) => {
+        console.log(event.target, event.target.index)
+        this.setState((state)=> {
+            return{items: state.items.filter((items, index)=> index!==Number(remove))}
+        })
+    }
+
     
     render(){
         return(
             <>
-            <ul>{this.state.items.map((items, index) => <li key={index}>{items}</li>)}</ul>
+            <ul>{this.state.items.map((item, index)=><li key={item + index}>{item}<button type='button' onClick={(event)=>{this.handleRemove(event, index)}} >Remove</button></li>)}</ul>
             <input name='todo' type='text' onChange={this.handleInput} value={this.state.newList} ></input>
         <button type='button' onClick={this.handleBtn} disabled={this.state.addBtn}>Add </button>
          <button onClick={this.handleBtnReset}>reset</button>
