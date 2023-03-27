36a08408dc6916559a7e8b1e54e4b0fa21a0e9df
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-03-27 18:17:08 +0200

    nuovo

--- new/src/App.js
+++ new/src/App.js
@@ -2,6 +2,8 @@ import { Hello } from "./Hello";
 import React from "react";
 import { Welcome } from "./Welcome";
 import {Counter } from "./Counter"
+import ClickCounter from "./ClickCounter";
+ 
 
 export class App extends React.Component {
   render() {
@@ -10,6 +12,10 @@ export class App extends React.Component {
         <Hello />
         <Welcome name="John" age={29} />
         <Counter initialValue={5} incrementInterval={500} incrementAmount={2} />
+        <ClickCounter />
+        <ClickTracker />
+        <InteractiveWelcome/>
+        <Login onLogin={this.onLogin}  />
       </div>
     );
   }
