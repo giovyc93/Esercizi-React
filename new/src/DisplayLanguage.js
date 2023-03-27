5ceb25ca62d29a09e39533c086f5b09a0f54dc09
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-03-27 21:42:30 +0200

    nuovo

--- new/src/DisplayLanguage.js
+++ new/src/DisplayLanguage.js
@@ -1,14 +1,21 @@
-import React from "react" 
+import { useContext } from "react"
+import { LanguageContext } from "./LanguageContext"
 
-
-class DisplayLanguage extends React.Component{
-    render(){
-        return <div>
-            
-        </div>
+const Strings = {
+    en: {
+        HELLO : "Hello"
+    },
+    it: {
+        HELLO : "Ciao"
     }
-    
 }
-
+const DisplayLanguage = () => {
+  
+    const language = useContext(LanguageContext)
+    return (
+          <h1>{Strings[language].HELLO}</h1>
+             
+  )
+}
 
 export default DisplayLanguage
