9de9277b30e60a7785bf0430f2a0efd321354416
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-03-27 21:53:32 +0200

    nuovo

--- new/src/App.js
+++ new/src/App.js
@@ -18,57 +18,22 @@ import UseForm from "./UseForm";
 import CarDetails from "./CarDetails";
 import FilteredList from "./FilteredList";
 import "./App.css";
+import {Route, BrowserRouter, Routes} from "react-router-dom"
 
 
 
-export class App extends React.Component {
-  onLogin = (state) => {
-    console.log(state);
-  };
-
-  render() {
-    return (
-      <div>
-        <Hello />
-        <Welcome name="John" age={29} />
-        < ClickTracker />
-        <InteractiveWelcome />
-        <Login />
-        <Counter />
-        <ClickCounter onCounterChange = {(counter)=> {
-                         console.log(counter)
-                    }}/>
-        <ClickTracker />
-        <InteractiveWelcome />
-        <Login onLogin={this.onLogin} />
-        <UncontrolledLogin />
-        <TodoList
-          render={(items, handleRemove) => (
-            <ul>
-              {items.map((item, index) => (
-                <li key={item + index}>
-                  {item}
-                  <button type="button" onClick={(event) => handleRemove(event, index)}>
-                    Remove
-                  </button>
-                </li>
-              ))}
-            </ul>
-          )}
-        />
-          <GithubUser />
-        <LanguageComponent />
-        <Container title="this is a component composition " />
-        <Sum />
-        <GithubUserList />
-        <UseCounter />
-        <UseForm/>
-        <CarDetails
-          initialData={{ model: "FIAT 500", year: "2015", color: "Black" }}
-        />
-        <FilteredList />
-      </div>
-    );
+function App () {
+    
+  
+  return (
+      
+          <Routes>
+              <Route path="/" element={<Welcome name="Emanuele" />} />
+              
+          </Routes>
+          
+        
+      )
   }
-}
+
 export default App
