5c275151f03222fc10b0bedae49ca0ef857d2d93
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-03-27 20:08:04 +0200

    nuovo

--- new/src/App.js
+++ new/src/App.js
@@ -1,31 +1,34 @@
-import { Hello } from "./Hello";
+import Hello from "./Hello";
 import React from "react";
-import { Welcome } from "./Welcome";
-import {Counter } from "./Counter"
+import Welcome from "./Welcome";
+import Counter from "./Counter"
 import UncontrolledLogin from "./UncrontrolledLogin";
+import Container from "./Container";
+import Login from "./Login";
 
 import TodoList from "./TodoList";
 
 export class App extends React.Component {
   onLogin = (state) => {
-		console.log(state);
-	};
+    console.log(state);
+  };
 
   render() {
     return (
       <div>
         <Hello />
         <Welcome name="John" age={29} />
-       < ClickTracker />
-       <InteractiveWelcome/>
-       <Login />
+        < ClickTracker />
+        <InteractiveWelcome />
+        <Login />
         <Counter initialValue={5} incrementInterval={500} incrementAmount={2} />
         <ClickCounter />
         <ClickTracker />
-        <InteractiveWelcome/>
-        <Login onLogin={this.onLogin}  />
-        <UncontrolledLogin/>
+        <InteractiveWelcome />
+        <Login onLogin={this.onLogin} />
+        <UncontrolledLogin />
         <TodoList />
+        <Container />
       </div>
     );
   }
